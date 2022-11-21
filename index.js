16a8814c2f1c4c90238520addeb724c8a167329c
CindyMisoi <crmisoi2k@gmail.com>
2022-11-21 12:22:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+
+
+function addingEventListener(){
+    input.addEventListener('click' ,function(event)
+
+    
+        {alert("I was clicked!");
+});
 }
+
